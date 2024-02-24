473ee182c0a2b80b33c1a228ce38b8aaa66b9355
Dominique <dominiqueliebens@telenet.be>
2024-02-24 20:21:06 +0100

    added new flutter base files

---
+++ flutter/base_values.dart
@@ -0,0 +1,39 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppValues {
+  void setServerUrl(String serverUrl) async {
+    SharedPreferences sharedPref = await SharedPreferences.getInstance();
+    sharedPref.setString("server_url", serverUrl);
+  }
+
+  void setServerPort(String serverPort) async {
+    SharedPreferences sharedPref = await SharedPreferences.getInstance();
+    sharedPref.setString("server_port", serverPort);
+  }
+
+  void setDefaultLanguage(String selectedLanguage) async {
+    SharedPreferences sharedPref = await SharedPreferences.getInstance();
+    sharedPref.setString("DefaultLanguage", selectedLanguage);
+  }
+
+  Future<String> getServerUrl() async {
+    SharedPreferences sharedPref = await SharedPreferences.getInstance();
+    return sharedPref.getString("server_url") ?? "";
+  }
+
+  Future<String> getServerPort() async {
+    SharedPreferences sharedPref = await SharedPreferences.getInstance();
+    return sharedPref.getString("server_port") ?? "";
+  }
+
+  Future<String> getDefaultLanguage() async {
+    SharedPreferences sharedPref = await SharedPreferences.getInstance();
+    return sharedPref.getString("DefaultLanguage") ?? "English";
+  }
+
+  void resetPreferences() async {
+    SharedPreferences sharedPref = await SharedPreferences.getInstance();
+    sharedPref.remove("server_url");
+    sharedPref.remove("server_port");
+  }
+}
